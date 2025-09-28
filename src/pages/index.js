3d0cb0facfc2c9add72afb641923a0d28e236a39
OTabara <doumoutabara@gmail.com>
2025-09-28 21:07:06 +0200

    Update index.js
    modification du CV

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
 
             {/* Bouton de téléchargement du CV */}
             <motion.a
-              href="/cv.pdf"
+              href="/CV_BI.pdf"
               download
               className="mt-6 inline-block bg-blue-600 text-white px-8 py-3 rounded-lg shadow-md hover:bg-blue-700 transition"
               whileHover={{ scale: 1.05 }}
